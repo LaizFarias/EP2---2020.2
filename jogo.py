f99fcefd4fb14e4a1bb90760df19ef3095aed0ca
LaizFarias <81171500+LaizFarias@users.noreply.github.com>
2021-11-07 22:03:22 -0300

    jogo

--- jogo.py
+++ jogo.py
@@ -17,23 +17,23 @@ print("\nVamos começar!!!")
 
 def colorir(frase, cor):
     # Tabela de cores - Usadas pela função "colorir"
+    BOLD    = "\033[;1m"
     RED   = "\033[1;31m"  
     BLUE  = "\033[1;34m"
-    CYAN  = "\033[1;36m"
     YELLOW  = "\033[1;33m"
+    CYAN  = "\033[1;36m"
     GREEN = "\033[0;32m"
     RESET = "\033[0;0m"
-    BOLD    = "\033[;1m"
     REVERSE = "\033[;7m"
-    dicionario = {
+    cores = {
         'vermelho': RED,
         'azul': BLUE,
         'ciano': CYAN,
         'amarelo': YELLOW,
         'verde': GREEN,
-        'negrito': BOLD,
+        'preto': BOLD,
     }
-    return dicionario[cor] + frase + RESET
+    return cores[cor] + frase + RESET
 
 
 comeca = input("Você quer jogar? (sim/não)").strip().lower()
